d46009ae0cad32226813975b7ca8bca42d5cbef4
veritius <veritiusgaming@gmail.com>
2024-07-23 17:32:37 +1000

    save

--- core/src/node.rs
+++ core/src/node.rs
@@ -1,57 +1,10 @@
 use core::ops::{Deref, DerefMut};
 use super::*;
 
-/// A generator for [`NodeId`] values.
-pub struct NodeIdGenerator {
-    #[cfg(not(feature="use_atomics"))]
-    idx: usize,
-
-    #[cfg(feature="use_atomics")]
-    idx: portable_atomic::AtomicUsize,
-}
-
-impl Default for NodeIdGenerator {
-    fn default() -> Self {
-        Self {
-            #[cfg(not(feature="use_atomics"))]
-            idx: usize::default(),
-
-            #[cfg(feature="use_atomics")]
-            idx: portable_atomic::AtomicUsize::default(),
-        }
-    }
-}
-
-impl NodeIdGenerator {
-    /// Generate a new [`NodeId`] for use.
-    pub fn next(&mut self) -> NodeId {
-        #[cfg(not(feature="use_atomics"))] {
-            let v = self.idx;
-            self.idx += 1;
-            return NodeId(v);
-        }
-
-        #[cfg(feature="use_atomics")] {
-            let v = *self.idx.get_mut();
-            self.idx.get_mut();
-            return NodeId(v);
-        }
-    }
-
-    /// Generate a new [`NodeId`] for use using atomic operations.
-    /// Since this uses atomic operations, this can be done with an immutable borrow.
-    #[cfg(feature="use_atomics")]
-    pub fn next_atomic(&self) -> NodeId {
-        use core::sync::atomic::Ordering;
-        let v = self.idx.fetch_add(1, Ordering::AcqRel);
-        return NodeId(v);
-    }
-}
-
 /// An identifier for a [`Node`].
 /// Unique only to ids generated with the same [`NodeIdGenerator`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct NodeId(usize);
+pub struct NodeId(pub usize);
 
 /// A node type.
 pub trait Node: Dirty {
