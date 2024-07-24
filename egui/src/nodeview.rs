8b473c42b5041d47c60533cb72159419c77eb93f
veritius <veritiusgaming@gmail.com>
2024-07-24 12:37:02 +1000

    nodeview tuff

--- egui/src/nodeview.rs
+++ egui/src/nodeview.rs
@@ -3,6 +3,28 @@
 use noodle_core::*;
 
 /// A builder for a node view.
-pub struct NodeViewBuilder<'a> {
-    node: &'a mut dyn Node,
+pub struct NodeViewBuilder<'a, N: Node> {
+    node: &'a mut N,
+
+    title: Option<&'a str>,
+}
+
+impl<'a, N: Node> NodeViewBuilder<'a, N> {
+    /// Creates a new [`NodeViewBuilder`].
+    pub fn new(node: &'a mut N) -> Self {
+        Self {
+            node,
+
+            title: None,
+        }
+    }
+
+    /// Use a specific name as the title of the node.
+    /// If unset (`None`), the node's discriminator is used instead.
+    /// 
+    /// Defaults to `None` (the node's discriminator).
+    pub fn title(mut self, name: Option<&'a str>) -> Self {
+        self.title = name;
+        return self;
+    }
 }
