1e30a10855d585e6446dcf22fc63f392bcfeecb1
veritius <veritiusgaming@gmail.com>
2024-07-25 13:38:00 +1000

    blanket valuelike impl

--- core/src/value.rs
+++ core/src/value.rs
@@ -5,10 +5,17 @@ use std::{any::Any, sync::Arc};
 #[repr(transparent)]
 pub struct Value(ValueInner);
 
-impl<T: Valuelike> From<T> for Value {
+impl Value {
+    /// Attempts to cast the [`Value`] to [`T`]. Fails if the value is not of type `T`.
     #[inline(always)]
-    fn from(value: T) -> Self {
-        Self(ValueInner::from(value))
+    pub fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
+        self.0.downcast::<T>()
+    }
+
+    /// Creates a new [`Value`] holding `value`.
+    #[inline(always)]
+    pub fn new<T: Valuelike>(value: T) -> Self {
+        Self(ValueInner::new(value))
     }
 }
 
@@ -19,24 +26,22 @@ impl AsRef<dyn Valuelike> for Value {
     }
 }
 
-impl Value {
-    /// Attempts to cast the [`Value`] to [`T`]. Fails if the value is not of type `T`.
-    #[inline(always)]
-    pub fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
-        self.0.downcast::<T>()
-    }
-}
-
 /// A type that can be put in a [`Value`].
 pub trait Valuelike: Any + Send + Sync + 'static {}
+
+impl<T> Valuelike for T where T: Any + Send + Sync + 'static {}
 
 #[derive(Clone)]
 struct ValueInner {
     value: Arc<dyn Valuelike>,
 }
 
-impl<T: Valuelike> From<T> for ValueInner {
-    fn from(value: T) -> Self {
+impl ValueInner {
+    fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
+        <dyn Any>::downcast_ref(&self.value).ok_or(TypeCastError)
+    }
+
+    fn new<T: Valuelike>(value: T) -> Self {
         Self {
             value: Arc::new(value),
         }
@@ -47,12 +52,6 @@ impl AsRef<dyn Valuelike> for ValueInner {
     #[inline(always)]
     fn as_ref(&self) -> &dyn Valuelike {
         self.value.as_ref()
-    }
-}
-
-impl ValueInner {
-    fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
-        <dyn Any>::downcast_ref(&self.value).ok_or(TypeCastError)
     }
 }
 
