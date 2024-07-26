f03cb5303102a342f10a0d137d2b205f71e38127
veritius <veritiusgaming@gmail.com>
2024-07-26 10:41:46 +1000

    remove redundant stuff

--- core/src/graph.rs
+++ core/src/graph.rs
@@ -21,17 +21,6 @@ pub trait Graph<N: Node> {
     /// Returns `None` if an estimate cannot be obtained.
     fn node_count(&self) -> Option<usize>;
 
-    /// Reserve space for at least `amt` nodes.
-    /// Does nothing if capacity is already sufficient.
-    fn reserve_nodes(&mut self, amt: usize);
-
-    /// Reserve space for at least `amt` nodes.
-    /// Does nothing if capacity is already sufficient.
-    /// 
-    /// Unlike [`reserve_nodes`](Self::reserve_nodes), this will not
-    /// deliberately over-allocate to speculatively avoid frequent allocations.
-    fn reserve_nodes_exact(&mut self, amt: usize);
-
     /// Inserts a link between a pair of sockets on two nodes.
     fn insert_link(&mut self, id: LinkId) -> Result<(), WouldCycle>;
 
@@ -45,31 +34,10 @@ pub trait Graph<N: Node> {
     /// Returns `None` if an estimate cannot be obtained.
     fn link_count(&self) -> Option<usize>;
 
-    /// Reserve space for at least `amt` nodes.
-    /// Does nothing if capacity is already sufficient.
-    fn reserve_links(&mut self, amt: usize);
-
-    /// Reserve space for at least `amt` nodes.
-    /// Does nothing if capacity is already sufficient.
-    /// 
-    /// Unlike [`reserve_nodes`](Self::reserve_nodes), this will not
-    /// deliberately over-allocate to speculatively avoid frequent allocations.
-    fn reserve_links_exact(&mut self, amt: usize);
-
     /// Solve for the output of `node` with the given [`OutputMask`].
     fn solve_node(
         &mut self,
         node: NodeId,
         outputs: OutputMask,
     ) -> Result<SocketValues, ()>;
-}
-
-/// A [`Graph`] that stores an internal cache.
-pub trait CachingGraph<N>
-where
-    Self: Graph<N>,
-    N: Node,
-{
-    /// Clear any cached output of a [`Node`] and any of its children.
-    fn clear_cached_output(&mut self, node: NodeId);
 }
