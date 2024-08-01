3a4f964dc48c2aed419473b6c7b38a7d76ec0800
veritius <veritiusgaming@gmail.com>
2024-08-01 10:57:21 +1000

    move the walker trait down a bit

--- core/src/traversal.rs
+++ core/src/traversal.rs
@@ -1,14 +1,5 @@
 use crate::*;
 
-/// A traversal state that doesn't keep a borrow of the graph, permitting mutable access to the graph.
-pub trait Walker {
-    /// Context given to [`next`](Walker::next) to progress.
-    type Context;
-
-    /// Walks to the next node in the graph.
-    fn next(&mut self, graph: Self::Context) -> Option<NodeId>;
-}
-
 /// Returns a [`Walker`] over the dependencies of a given node.
 pub trait WalkDependencies<N>
 where
@@ -33,4 +24,13 @@ where
 
     /// Returns an object that can be used to iterate over the nodes dependent on `node`.
     fn walk_dependents(&self, node: NodeId) -> Option<Self::Walker>;
+}
+
+/// A traversal state that doesn't keep a borrow of the graph, permitting mutable access to the graph.
+pub trait Walker {
+    /// Context given to [`next`](Walker::next) to progress.
+    type Context;
+
+    /// Walks to the next node in the graph.
+    fn next(&mut self, graph: Self::Context) -> Option<NodeId>;
 }
