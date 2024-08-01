90f6cd008d160e807a9c79135ed8a8f3170fbe67
veritius <veritiusgaming@gmail.com>
2024-08-01 10:56:49 +1000

    improve walker trait

--- core/src/traversal.rs
+++ core/src/traversal.rs
@@ -1,13 +1,12 @@
 use crate::*;
 
 /// A traversal state that doesn't keep a borrow of the graph, permitting mutable access to the graph.
-pub trait Walker<G, N>
-where
-    G: Graph<N> + ?Sized,
-    N: Node,
-{
+pub trait Walker {
+    /// Context given to [`next`](Walker::next) to progress.
+    type Context;
+
     /// Walks to the next node in the graph.
-    fn next(&mut self, graph: &G) -> Option<NodeId>;
+    fn next(&mut self, graph: Self::Context) -> Option<NodeId>;
 }
 
 /// Returns a [`Walker`] over the dependencies of a given node.
@@ -17,7 +16,7 @@ where
     N: Node,
 {
     /// The returned walker.
-    type Walker: Walker<Self, N>;
+    type Walker: Walker;
 
     /// Returns an object that can be used to iterate over the dependencies of a `node`.
     fn walk_dependencies(&self, node: NodeId) -> Option<Self::Walker>;
@@ -30,7 +29,7 @@ where
     N: Node,
 {
     /// The returned walker.
-    type Walker: Walker<Self, N>;
+    type Walker: Walker;
 
     /// Returns an object that can be used to iterate over the nodes dependent on `node`.
     fn walk_dependents(&self, node: NodeId) -> Option<Self::Walker>;
