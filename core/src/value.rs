04ebfb02bbacf27b1cc20dfe7daf0a71849ac41d
veritius <veritiusgaming@gmail.com>
2024-08-02 13:48:05 +1000

    a

--- core/src/value.rs
+++ core/src/value.rs
@@ -1,6 +1,4 @@
-use core::any::Any;
-use std::ops::Deref;
-use alloc::sync::Arc;
+use core::{any::Any, ops::Deref};
 
 /// A reference to a value.
 #[derive(Clone, Copy)]
@@ -10,6 +8,7 @@ pub struct ValueRef<'a> {
 
 impl<'a> ValueRef<'a> {
     /// Returns a new [`ValueRef`] for `value`.
+    #[inline]
     pub fn new<T: Any>(value: &'a T) -> ValueRef<'a> {
         Self { value }
     }
@@ -22,8 +21,8 @@ impl<'a> ValueRef<'a> {
 
     /// Tries to cast the inner borrow to `T`.
     #[inline]
-    pub fn downcast<T: Any>(&'a self) -> Option<&'a T> {
-        self.value.downcast_ref()
+    pub fn downcast<T: Any>(&'a self) -> Result<&'a T, TypeCastError> {
+        self.value.downcast_ref().ok_or(TypeCastError)
     }
 }
 
@@ -35,60 +34,10 @@ impl Deref for ValueRef<'_> {
     }
 }
 
-/// A type-erased reference-counted value used in node graph calculations.
-#[derive(Clone)]
-#[repr(transparent)]
-pub struct Value(ValueInner);
-
-impl Value {
-    /// Attempts to cast the [`Value`] to `T`. Fails if the value is not of type `T`.
-    #[inline(always)]
-    pub fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
-        self.0.downcast::<T>()
-    }
-
-    /// Creates a new [`Value`] holding `value`.
-    #[inline(always)]
-    pub fn new<T: Valuelike>(value: T) -> Self {
-        Self(ValueInner::new(value))
-    }
-}
-
-impl AsRef<dyn Valuelike> for Value {
-    #[inline(always)]
-    fn as_ref(&self) -> &dyn Valuelike {
-        self.0.value.as_ref()
-    }
-}
-
 /// A type that can be put in a [`Value`].
 pub trait Valuelike: Any + Send + Sync + 'static {}
 
 impl<T> Valuelike for T where T: Any + Send + Sync + 'static {}
-
-#[derive(Clone)]
-struct ValueInner {
-    value: Arc<dyn Valuelike>,
-}
-
-impl ValueInner {
-    fn downcast<'a, T: Valuelike>(&'a self) -> Result<&'a T, TypeCastError> {
-        <dyn Any>::downcast_ref(&self.value).ok_or(TypeCastError)
-    }
-
-    fn new<T: Valuelike>(value: T) -> Self {
-        Self {
-            value: Arc::new(value),
-        }
-    }
-}
-
-impl AsRef<dyn Valuelike> for ValueInner {
-    #[inline(always)]
-    fn as_ref(&self) -> &dyn Valuelike {
-        self.value.as_ref()
-    }
-}
 
 /// Returned when attempting to cast a [`Value`] to a concrete type fails.
 #[derive(Debug, Clone, Copy)]
